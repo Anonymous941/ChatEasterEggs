8a72d7c661b930ccee1014ea51664528d0ea504f
Anonymous941 <36797492+Anonymous941@users.noreply.github.com>
2021-07-31 11:31:34 -0400

    Create stackexchangeeastereggs.user.js

---
+++ stackexchangeeastereggs.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         Chat easter eggs
+// @description  Enables moderation tools in private teams.
+// @author       Anonymous
+// @version      1.0.0
+// @run-at document-end
+//
+// @include      *://chat.stackexchange.com/rooms/*
+// @include      *://chat.stackoverflow.com/rooms/*
+// @include      *://chat.meta.stackexchange.com/rooms/*
+//
+// @require      https://code.jquery.com/jquery-1.12.4.min.js
+// ==/UserScript==
+
+var script = $("<script>");
+$(document).ready(function() {
+  script.text("(" + function() {
+    $.getScript("https://cdn-chat.sstatic.net/chat/Js/eggs.js");
+    $.fn.preBind = function (type, data, fn) {
+      this.each(function () {
+        var $this = $(this);
+        $this.bind(type, data, fn);
+        var currentBindings = $._data(this, 'events')[type];
+        if ($.isArray(currentBindings)) {
+          currentBindings.unshift(currentBindings.pop());
+        }
+      });
+      return this;
+    };
+    function easterEggRun(messageText) {
+      Eggs.Cthulu(messageText);
+      Eggs.Assistant(messageText);
+      Eggs.Asteroids(messageText);
+      Eggs.Console(messageText);
+      Eggs.WOB(messageText);
+    }
+    $("#input").preBind('keydown', function (t) {
+      if (229 != t.which) {
+        switch ((38 == t.which || 40 == t.which), t.which) {
+          case 13:
+            t.shiftKey || easterEggRun($("#input").val());
+            break;
+        }
+      }
+    });
+  } + ")();");
+});
+$("head").append(script);
